8b5fe2292c721d71a29ff0e61ce5467b22841fe7
anna14pmm <anmenamor@uide.edu.ec>
2026-05-19 01:29:28 +0000

    wip: first commands and C code

---
+++ s2.sh
@@ -0,0 +1,22 @@
+# Revisar el manual de gcc, se puede revisar por líneas (flecha abajo) o por páginas (barra espaciadora)
+man gcc
+# Comando que me ayuda a revisar comandos puntuales para ser ingresando e indicando su función
+gcc --help
+# Compilar es de codigo fuente a maquina
+gcc hola.ec -o hola_binario
+
+file hola_binario
+
+file hola.c
+
+# Directorio home de cualquier usuario
+cd 
+# Me ayuda a ver la ruta
+pwd
+# Ruta absoluta: me indica la ruta desde el raíz hasta el directorio donde está mi archivo. Se puede colocar con clic derecho y copiar ruta.
+
+# Lista de archivos
+ls
+
+nl 
+# generar un espacio
